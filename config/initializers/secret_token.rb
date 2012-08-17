2685b9a4077a187516adf61c25289429b9b9e509
Paul Wittmann <paul@railslove.com>
2012-08-17 14:08:07 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlackSheep::Application.config.secret_token = '69cd931a189c9db45a09ea0380b0c2d6490dd29dfb5e99f4ab05882504b969d2a0fb1e7efd37384c076d893afc5d2e37c563f9914378ad59428321177e3bcaa6'
